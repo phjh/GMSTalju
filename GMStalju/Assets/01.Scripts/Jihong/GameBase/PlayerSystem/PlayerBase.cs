8768a8c0976e1253d3cc22195891d4e2fcab0d22
phjh <adsf8800@naver.com>
2023-10-12 19:01:58 +0900

    cm
    commit

--- GMStalju/Assets/01.Scripts/Jihong/GameBase/PlayerSystem/PlayerBase.cs
+++ GMStalju/Assets/01.Scripts/Jihong/GameBase/PlayerSystem/PlayerBase.cs
@@ -1,13 +1,15 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using Unity.Mathematics;
 
 public class PlayerBase : PlayerRoot
 {
     [Header("참조 컴포넌트")] 
     [SerializeField]
     protected GameObject moveUI;
+    [SerializeField] 
+    protected List<GameObject> moveUIButton;
     //[SerializeField]
     //protected Animator animator;
     //[SerializeField]
@@ -47,7 +49,7 @@ public class PlayerBase : PlayerRoot
     protected void Move(Vector2 dir)
     {
         Ray2D ray;
-        if (!Physics2D.BoxCast(transform.localPosition, transform.localScale, 45 * (float)moveDir, dir, TileSystem.tileSystem.multiplied) && leftMovements > 0)
+        if (!Physics2D.BoxCast(transform.localPosition + new Vector3(0,0.125f,0), transform.localScale - (new Vector3(0.1f,0.1f,0.1f)), 45 * (float)moveDir, dir, TileSystem.tileSystem.multiplied) && leftMovements > 0)
         {
             leftMovements--;
             StartCoroutine(MoveTo(new Vector2(transform.position.x + dir.x, transform.position.y + dir.y)));
@@ -58,11 +60,11 @@ public class PlayerBase : PlayerRoot
     {
         isMoving = true;
         moveUI.SetActive(false);
-        Quaternion rot = Quaternion.Euler(0, 0, 360 - ((int)moveDir * 45));
-        yield return transform.DORotate(rot.eulerAngles,Mathf.Abs(degreeInverter(transform.rotation.z) - degreeInverter(rot.z))/2);
+        //Quaternion rot = Quaternion.Euler(0, 0, 360 - ((int)moveDir * 45));
+        //yield return transform.DORotate(rot.eulerAngles,Mathf.Abs(degreeInverter(transform.rotation.z) - degreeInverter(rot.z))/2);
         yield return transform.DOMove(pos, Vector2.Distance(transform.position, pos) / speed, false);
-        yield return new WaitForSeconds(Mathf.Max(Mathf.Abs(transform.rotation.z - degreeInverter(rot.z)) / 2f,Vector2.Distance(transform.position, pos) / speed) + 0.45f);
-        moveUI.transform.rotation = quaternion.Euler(0,0,0);
+        yield return new WaitForSeconds((Vector2.Distance(transform.position, pos) / speed) + 0.45f);
+        //moveUI.transform.rotation = quaternion.Euler(0,0,0);
         moveUI.SetActive(true);
         isMoving = false;
     }
